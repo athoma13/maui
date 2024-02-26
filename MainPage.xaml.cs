d6f2eac642ceaf51b274ce1318d59c0a07d57aee
Arnaud Thomasse <arnaud@gadsolutions.com.au>
2024-02-26 16:17:02 +1100

    Permissions

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,34 @@
-﻿namespace maui;
+﻿using Microsoft.Maui.Handlers;
+
+namespace maui;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
+		Loaded += MainPage_Loaded;
+	}
+
+	private void MainPage_Loaded(object sender, EventArgs e)
+	{
+		var platformView = (wv.Handler as IWebViewHandler)?.PlatformView;
+
+		if (platformView != null)
+		{
+			platformView.CoreWebView2Initialized += (s, e) =>
+			{
+				var coreWebView2 = platformView.CoreWebView2;
+				if (coreWebView2 != null)
+				{
+					coreWebView2.PermissionRequested += (s, e) =>
+					{
+						// Allow all permissions request by the WebView. (of course, we need the app to have been granted those permissions first.)
+						e.State = Microsoft.Web.WebView2.Core.CoreWebView2PermissionState.Allow;
+					};
+				}
+			};
+		}
 	}
 }
 
